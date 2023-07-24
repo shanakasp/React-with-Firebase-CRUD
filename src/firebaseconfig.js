b826960b4e8fb96ef6a9977795f660b6afae1566
shanakasp <74164029+shanakasp@users.noreply.github.com>
2023-07-24 13:24:31 +0530

    changed 3 files

---
+++ src/firebaseconfig.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBej8DhYhS4IMsOxF9tbL4MZO-L5vsNwhc",
+  authDomain: "fir-react-01-46071.firebaseapp.com",
+  projectId: "fir-react-01-46071",
+  storageBucket: "fir-react-01-46071.appspot.com",
+  messagingSenderId: "709913101886",
+  appId: "1:709913101886:web:97c622783d80aa92ef336f",
+  measurementId: "G-0EGPPC33EY"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
