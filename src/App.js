363fc66d56230d767ed6dc744ec5ae8b10c6f76a
shanakasp <74164029+shanakasp@users.noreply.github.com>
2023-07-25 16:30:29 +0530

    modified App.js in src

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { useState, useEffect } from 'react';
+import { db } from './firebaseconfig';
+import {collection, getDocs} from "firebase/firestore"
 function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
-}
+      const [users, setUsers] = useState([]);
+     const usersCollectionRef = collection(db, "users")
+     useEffect(() => {
+      const getUsers = async () =>
+      {
+          const data = await getDocs(usersCollectionRef);
+          setUsers(data.docs.map((doc) => ({...doc.data(), id: doc.id})))
+      }
+      getUsers();
+     }, []);
+
+   return   ( 
+   
+   <div className="App"> 
+   {users.map((user) =>
+   {
+    return<div> <h1>Name: {user.name}</h1>
+    <h1>Age: {user.age}</h1></div>
+   }
+   )}
+
+   </div>
+  
+);}
 
 export default App;
